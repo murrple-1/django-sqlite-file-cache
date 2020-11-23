68fc644021e4eb35b45cb314a82f434fbceece45
Murray Christopherson <murraychristopherson@gmail.com>
2020-11-23 15:51:48 -0800

    more tests

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -22,13 +22,6 @@ class TestCache(unittest.TestCase):
 
         self.assertEqual(cache.get('my_key'), 'value')
 
-    def test_get_without_file(self):
-        cache = SQLiteFileCache(self.location, {})
-
-        os.unlink(self.location)
-
-        self.assertIsNone(cache.get('my_key'))
-
     def test_add(self):
         cache = SQLiteFileCache(self.location, {})
 
@@ -92,7 +85,7 @@ class TestCache(unittest.TestCase):
         cache = SQLiteFileCache(self.location, {
             'OPTIONS': {
                 'MAX_ENTRIES': 10,
-            }
+            },
         })
 
         self.assertEqual(cache._max_entries, 10)
@@ -113,6 +106,67 @@ class TestCache(unittest.TestCase):
         count = cur.fetchone()[0]
         conn.close()
         self.assertLess(count, cache._max_entries)
+
+    def test_cull_frequency(self):
+        cache = SQLiteFileCache(self.location, {
+            'OPTIONS': {
+                'MAX_ENTRIES': 10,
+                'CULL_FREQUENCY': 0,
+            },
+        })
+
+        self.assertEqual(cache._max_entries, 10)
+
+        for i in range(cache._max_entries):
+            cache.set(f'my_key{i}', 'value')
+
+        conn = sqlite3.connect(self.location)
+        cur = conn.execute('''SELECT COUNT(key) FROM cache_entries''')
+        count = cur.fetchone()[0]
+        conn.close()
+        self.assertEqual(count, cache._max_entries)
+
+        cache.set('a_key', 'value')
+
+        conn = sqlite3.connect(self.location)
+        cur = conn.execute('''SELECT COUNT(key) FROM cache_entries''')
+        count = cur.fetchone()[0]
+        conn.close()
+        self.assertEqual(count, 1)
+
+    def test_expiry(self):
+        cache = SQLiteFileCache(self.location, {
+            'TIMEOUT': 0,
+        })
+
+        cache.set('my_key', 'value')
+
+        self.assertFalse(cache.has_key('my_key'))
+
+        cache.set('my_key', 'value')
+
+        self.assertIsNone(cache.get('my_key'))
+
+        cache.set('my_key', 'value')
+
+        self.assertFalse(cache.touch('my_key'))
+
+        cache.set('my_key', 'value')
+
+        self.assertFalse(cache.delete('my_key'))
+
+    def test_missing_file(self):
+        cache = SQLiteFileCache(self.location, {})
+
+        os.unlink(self.location)
+
+        self.assertIsNone(cache.get('my_key'))
+
+        self.assertFalse(cache.touch('my_key'))
+
+        self.assertFalse(cache.has_key('my_key'))
+
+        cache.clear()
 
 
 if __name__ == '__main__':
