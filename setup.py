430423f3e39c743f404f3b6a3a32147a9d3631b1
Murray Christopherson <murraychristopherson@gmail.com>
2020-11-24 16:39:34 -0800

    upping version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='django-sqlite-file-cache',
-    version='0.4.1',
+    version='0.4.2',
     description='A Django cache backend which has a similar API to FileBasedCache, but reduces the number of inodes used',
     long_description=long_description,
     long_description_content_type='text/markdown',
